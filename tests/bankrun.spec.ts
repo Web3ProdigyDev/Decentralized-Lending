326454bbe15254f4c553e7b2f7917332b037c2ac
Godwin Adakonye John <gj09042003@gmail.com>
2025-07-22 05:25:19 +0100

    Finished initializing and ready to add tests to the test file

--- tests/bankrun.spec.ts
+++ tests/bankrun.spec.ts
@@ -1,18 +1,23 @@
 import { describe, it } from 'node:test';
 import IDL from '../target/idl/lending.json';
 import { Lending } from '../target/types/lending';
-import { ProgramTestContext, startAnchor } from 'solana-bankrun';
+import { BanksClient, ProgramTestContext, startAnchor } from 'solana-bankrun';
 import { Program } from '@coral-xyz/anchor';
-import { Connection, PublicKey } from '@solana/web3.js';
+import { Connection, Keypair, PublicKey } from '@solana/web3.js';
 import { BankrunProvider } from 'anchor-bankrun';
 import { PythSolanaReceiver } from '@pythnetwork/pyth-solana-receiver';
 import { BankrunContextWrapper } from '../bankrun-utils/bankrunConnection';
-import { Context } from 'mocha';
+import { createMint } from '@solana/spl-token';
 
 describe("Lending Smart Contract Test", async () => {
     let context: ProgramTestContext;
     let provider: BankrunProvider;
     let BankrunContextWrapper: BankrunContextWrapper;
+    let program: Program<Lending>;
+    let banksClient: BanksClient;
+    let signer: Keypair;
+    let usdcBankAccount: PublicKey;
+    let solBankAccount: PublicKey;
 
     const pyth = new PublicKey("Fw1ETanDZafof7xEULsnq9UY6o71Tpds89tNwPkWLb1v")
 
@@ -24,11 +29,57 @@ describe("Lending Smart Contract Test", async () => {
         [{name: "Lending", programId: new PublicKey(IDL.address) }],
     [{ address: pyth, info: AccountInfo }]
 );
-provider = new BankrunProvider(context);
+    provider = new BankrunProvider(context);
 
-const SOL_PRICE_FEED_ID = "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a";
+    const SOL_PRICE_FEED_ID = "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a";
 
-BankrunContextWrapper = new BankrunContextWrapper(context);
+    BankrunContextWrapper = new BankrunContextWrapper(context);
 
-const pythSolanaReceiver = new PythSolanaReceiver();
+    const connection = BankrunContextWrapper.connection.toConnection();
+
+    {const pythSolanaReceiver = new PythSolanaReceiver({
+        connection,
+        wallet: provider.wallet,
+    });
+    const solUsdPriceFeedAccount = pythSolanaReceiver.getPriceFeedAccountAddress(
+        0, 
+        SOL_PRICE_FEED_ID
+    );
+    const feedAccountInfo = await devnetConnection.getAccountInfo(
+        solUsdPriceFeedAccount
+    );
+
+    context.setAccount(solUsdPriceFeedAccount, feedAccountInfo);
+    program = new Program<Lending>( IDL as Lending, provider);
+
+    banksClient = context.banksClient;
+    signer = provider.wallet.payer;
+
+    const mintUSDC = await createMint(
+        banksClient,
+        signer, 
+        signer.publicKey, 
+        null,
+        2
+    );
+
+    const mintSol = await createMint(
+        banksClient,
+        signer, 
+        signer.publicKey, 
+        null,
+        2
+    );
+
+    [usdcBankAccount] = PublicKey.findProgramAddressSync(
+        [Buffer.from("treasury", mintUSDC.toBuffer())],
+        program.programId
+    );
+
+    [solBankAccount] = PublicKey.findProgramAddressSync(
+        [Buffer.from("treasury", mintSol.toBuffer())],
+        program.programId
+    );
+
 });
+
